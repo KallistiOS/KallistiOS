e46a975583789bd60000c6e63e99c17be43e2517
sizious <sizious@gmail.com>
2018-10-15 18:45:20 +0200

    Fix adventure example for MinGW-w64/MSYS2.

--- examples/dreamcast/conio/adventure/hdr.h
+++ examples/dreamcast/conio/adventure/hdr.h
@@ -55,14 +55,20 @@
 
 /* hdr.h: included by c advent files */
 #ifdef SETUP
+
 #ifdef __MINGW32__
-#ifndef _MINGW_W64
+#ifdef _MINGW_W64
+/* MinGW-w64/MSYS2 */
+typedef unsigned long u_long;
+#else
+/* MinGW/MSYS */
 #define _BSD_SOURCE
 #include <sys/bsdtypes.h>
+#endif
 #include <stdlib.h>
 #define srandom srand
-#endif
-#endif
+#endif /* __MINGW32__ */
+
 #include <sys/types.h>
 #include <signal.h>
 #else
