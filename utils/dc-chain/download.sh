7d6425f93569009c3ef7e8de4f0809585d34ac7a
sizious <sizious@gmail.com>
2018-10-23 15:22:12 +0200

    Improving `dc-chain` build system: - Components versions are now set directly in the `dc-chain/Makefile`. No need to modify `sh` scripts anymore. - In the `Makefile`, the **GNU Debugger** (`gdb`) and **Insight** are downloaded through `wget` or `curl`, depending on what is available on the system. This mechanism is a copy of the one used in `download.sh`. - Code refactoring in `download.sh`. - Code improvement in `cleanup.sh`.

--- utils/dc-chain/download.sh
+++ utils/dc-chain/download.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# These version numbers are all that should ever have to be changed.
-export GCC_VER=4.7.3
-export BINUTILS_VER=2.31.1
-export NEWLIB_VER=2.0.0
-export GMP_VER=4.3.2
-export MPFR_VER=2.4.2
-export MPC_VER=0.8.1
+# Getting versions defined in Makefile
+source ./version.sh
 
 while [ "$1" != "" ]; do
     PARAM=`echo $1 | awk -F= '{print $1}'`
@@ -33,52 +28,42 @@ while [ "$1" != "" ]; do
     shift
 done
 
-# Download everything.
+# Retrieve the web downloader program available in this system.
 if command -v wget >/dev/null 2>&1; then
-    echo "Downloading binutils-$BINUTILS_VER..."
-    wget -c ftp://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.xz || exit 1
-    echo "Downloading GCC $GCC_VER..."
-    wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.bz2 || exit 1
-    echo "Downloading Newlib $NEWLIB_VER..."
-    wget -c ftp://sourceware.org/pub/newlib/newlib-$NEWLIB_VER.tar.gz || exit 1
-
-    if [ -n "$GMP_VER" ]; then
-        echo "Downloading GMP $GMP_VER..."
-        wget -c ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-$GMP_VER.tar.bz2 || exit 1
-    fi
-
-    if [ -n "$MPFR_VER" ]; then
-        echo "Downloading MPFR $MPFR_VER..."
-        wget -c ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-$MPFR_VER.tar.bz2 || exit 1
-    fi
-
-    if [ -n "$MPC_VER" ]; then
-        echo "Downloading MPC $MPC_VER..."
-        wget -c ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-$MPC_VER.tar.gz || exit 1
-    fi
+	WEB_DOWNLOADER="wget -c"
 elif command -v curl >/dev/null 2>&1; then
-    echo "Downloading Binutils $BINUTILS_VER..."
-    curl -C - -O ftp://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.xz || exit 1
-    echo "Downloading GCC $GCC_VER..."
-    curl -C - -O ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.bz2 || exit 1
-    echo "Downloading Newlib $NEWLIB_VER..."
-    curl -C - -O ftp://sourceware.org/pub/newlib/newlib-$NEWLIB_VER.tar.gz || exit 1
-
-    if [ -n "$GMP_VER" ]; then
-        echo "Downloading GMP $GMP_VER..."
-        curl -C - -O ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-$GMP_VER.tar.bz2 || exit 1
-    fi
-
-    if [ -n "$MPFR_VER" ]; then
-        echo "Downloading MPFR $MPFR_VER..."
-        curl -C - -O ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-$MPFR_VER.tar.bz2 || exit 1
-    fi
-
-    if [ -n "$MPC_VER" ]; then
-        echo "Downloading MPC $MPC_VER..."
-        curl -C - -O ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-$MPC_VER.tar.gz || exit 1
-    fi
+	WEB_DOWNLOADER="curl -C - -O"
+	IS_CURL=1
 else
-    echo >&2 "You must have either wget or cURL installed to use this script!"
-    exit 1
+	echo >&2 "You must have either wget or cURL installed to use this script!"
+	exit 1
 fi
+
+# Download everything.
+if [ -z "${CONFIG_GUESS_ONLY}" ]; then
+	echo "Downloading Binutils $BINUTILS_VER..."
+	${WEB_DOWNLOADER} ftp://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.xz || exit 1
+
+	echo "Downloading GCC $GCC_VER..."
+	${WEB_DOWNLOADER} ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.bz2 || exit 1
+
+	echo "Downloading Newlib $NEWLIB_VER..."
+	${WEB_DOWNLOADER} ftp://sourceware.org/pub/newlib/newlib-$NEWLIB_VER.tar.gz || exit 1
+
+	if [ -n "$GMP_VER" ]; then
+		echo "Downloading GMP $GMP_VER..."
+		${WEB_DOWNLOADER} ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-$GMP_VER.tar.bz2 || exit 1
+	fi
+
+	if [ -n "$MPFR_VER" ]; then
+		echo "Downloading MPFR $MPFR_VER..."
+		${WEB_DOWNLOADER} ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-$MPFR_VER.tar.bz2 || exit 1
+	fi
+
+	if [ -n "$MPC_VER" ]; then
+		echo "Downloading MPC $MPC_VER..."
+		${WEB_DOWNLOADER} ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-$MPC_VER.tar.gz || exit 1
+	fi
+fi
+
+echo "Done!"
--- utils/dc-chain/unpack.sh
+++ utils/dc-chain/unpack.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# These version numbers are all that should ever have to be changed.
-export GCC_VER=4.7.3
-export BINUTILS_VER=2.31.1
-export NEWLIB_VER=2.0.0
-export GMP_VER=4.3.2
-export MPFR_VER=2.4.2
-export MPC_VER=0.8.1
+# Getting versions defined in Makefile
+source ./version.sh
 
 while [ "$1" != "" ]; do
     PARAM=`echo $1 | awk -F= '{print $1}'`
@@ -38,22 +33,32 @@ rm -rf binutils-$BINUTILS_VER gcc-$GCC_VER newlib-$NEWLIB_VER
 rm -rf gmp-$GMP_VER mpfr-$MPFR_VER mpc-$MPC_VER
 
 # Unpack everything.
+echo "Unpacking Binutils $BINUTILS_VER..."
 tar xf binutils-$BINUTILS_VER.tar.xz || exit 1
+
+echo "Unpacking GCC $GCC_VER..."
 tar xf gcc-$GCC_VER.tar.bz2 || exit 1
+
+echo "Unpacking Newlib $NEWLIB_VER..."
 tar xf newlib-$NEWLIB_VER.tar.gz || exit 1
 
 # Unpack the GCC dependencies and move them into their required locations.
 if [ -n "$GMP_VER" ]; then
+	echo "Unpacking GMP $GMP_VER..."
     tar jxf gmp-$GMP_VER.tar.bz2 || exit 1
     mv gmp-$GMP_VER gcc-$GCC_VER/gmp
 fi
 
 if [ -n "$MPFR_VER" ]; then
+	echo "Unpacking MPFR $MPFR_VER..."
     tar jxf mpfr-$MPFR_VER.tar.bz2 || exit 1
     mv mpfr-$MPFR_VER gcc-$GCC_VER/mpfr
 fi
 
 if [ -n "$MPC_VER" ]; then
+	echo "Unpacking MPC $MPC_VER..."
     tar zxf mpc-$MPC_VER.tar.gz || exit 1
     mv mpc-$MPC_VER gcc-$GCC_VER/mpc
 fi
+
+echo "Done!"
