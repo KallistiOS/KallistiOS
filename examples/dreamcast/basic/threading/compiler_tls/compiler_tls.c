2a2b8d029125795d41f0df92336e6396e26de2fb
Falco Girgis <gyrovorbis@gmail.com>
2023-04-25 23:16:24 -0400

    Added more test coverage to C/C++ TLS example
    - Added coverage for main thread's TLS data getting flexed
    - Added arbitary alignments for flexing tdata and tbss alignment

--- examples/dreamcast/basic/threading/compiler_tls/compiler_tls.c
+++ examples/dreamcast/basic/threading/compiler_tls/compiler_tls.c
@@ -9,12 +9,16 @@
    This example launches two threads that access variables
    placed in the TLS segment by the compiler. The compiler
    is then able to generate trivial lookups based on the GBR
-   register which holds the address to the current threads's
+   register which holds the address to the current thread's
    control block.
 
  */
 
 #include <kos.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdatomic.h>
 
 #if (__GNUC__ <= 4)
 /* GCC4 only supports using TLS with the __thread identifier,
@@ -25,9 +29,10 @@
 #define thread_local _Thread_local
 #endif
 
-extern thread_local uint32_t tbss_test = 0;
-extern thread_local uint32_t tdata_test = 5;
-
+static _Alignas(64) thread_local uint32_t tbss_test = 0;
+static _Alignas(128) thread_local char string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+static _Alignas(32) thread_local uint64_t tdata_test = 5;
+static volatile int errorCount = 0;
 
 /* Thread Function */
 void *thd(void *v) {
@@ -37,15 +42,30 @@ void *thd(void *v) {
     printf("Started Thread %d\n", id);
 
     for (i = 0; i < 5; i++){        
-        printf("Thread[%d]\tbss_test = 0x%lX\n", id, tbss_test);
+        printf("Thread[%d]\ttbss_test = 0x%lX\n", id, tbss_test);
         tbss_test++;
         thd_sleep(50);
     }
 
+    if(tbss_test != 5) {
+        printf("Thread[%d]\ttbss_test: %s\n", id, "FAILURE");
+        ++errorCount;
+    }
+
     for (i = 0; i < 5; i++){
-        printf("Thread[%d]\ttdata_test = 0x%lX\n", id, tdata_test);
+        printf("Thread[%d]\ttdata_test = 0x%llX\n", id, tdata_test);
         tdata_test++;
         thd_sleep(50);
+    }
+
+    if(tdata_test != 10) {
+        printf("Thread[%d]\ttdata_test: %s\n", id, "FAILURE");
+        ++errorCount;  
+    }
+
+    if(strcmp(string, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") != 0) {
+        printf("Incorrectly aligned string data!\n");
+        ++errorCount;
     }
 
     printf("Finished Thread %d\n", id);
@@ -54,22 +74,48 @@ void *thd(void *v) {
 
 /* The main program */
 int main(int argc, char **argv) {
-    const int thread_count = 2;
+    const int thread_count = 5;
 
     int i;
     kthread_t * threads[thread_count];   
 
     printf("Starting Threads\n");
-
     for(i = 0; i < thread_count; i++) {
-        threads[i] = thd_create(0, thd, (void *) i);
+        threads[i] = thd_create(0, thd, (void *)i);
     };
 
-    for(i = 0; i < thread_count; i++) {
-        int ret = (int) thd_join(threads[i], NULL);
-        printf("Thread[%d] Returned: %d\n", i, ret);
+    for (i = 0; i < 5; i++) {        
+        printf("Thread[%s]\ttbss_test = 0x%lX\n", "main", tbss_test);
+        tbss_test++;
+        thd_sleep(50);
+    }
+
+    if(tbss_test != 5) {
+        printf("Thread[%s]\ttbss_test: %s\n", "main", "FAILURE");
+        ++errorCount;
+    }
+
+    for (i = 0; i < 5; i++) {
+        printf("Thread[%s]\ttdata_test = 0x%llX\n", "main", tdata_test);
+        tdata_test++;
+        thd_sleep(50);
     }
     
+    if(tdata_test != 10) {
+        printf("Thread[%s]\ttdata_test: %s\n", "main", "FAILURE");
+        ++errorCount;
+    }
+
+    for(i = 0; i < thread_count; i++)
+        thd_join(threads[i], NULL);
+    
     printf("Threads Finished!\n");
-    return 0;
+    
+    if(!errorCount) {
+        printf("SUCCESS!\n");
+        return EXIT_SUCCESS;
+    } else {
+        printf("%d ERRORS!\n", errorCount);
+        return EXIT_FAILURE;
+    }
 }
