3bb34b585beec32a3ecfd60c9e91d6540527a01e
Falco Girgis <gyrovorbis@gmail.com>
2023-04-26 21:07:53 -0400

    RTC cleanup, documentation, setting date/time
    - Improved Doxygen documentation for RTC, adding more information on it
      and a new module
    - Added support for setting the date/time
    - Removed magic numbers
    - Added a #define for configuring retry attempts for read/write failures

--- kernel/arch/dreamcast/include/arch/rtc.h
+++ kernel/arch/dreamcast/include/arch/rtc.h
@@ -2,11 +2,13 @@
 
    arch/dreamcast/include/rtc.h
    (c)2000-2001 Megan Potter
+   (c)2023 Falco Girgis
 
 */
 
-/** \file   arch/rtc.h
-    \brief  Low-level real time clock functionality.
+/** \file    arch/rtc.h
+    \brief   Low-level real time clock functionality.
+    \ingroup rtc
 
     This file contains functions for interacting with the real time clock in the
     Dreamcast. Generally, you should prefer interacting with the higher level
@@ -23,29 +25,78 @@ __BEGIN_DECLS
 
 #include <time.h>
 
-/** \brief  Get the current date/time.
+/** \defgroup rtc Real-Time Clock
+    \brief Real-Time Clock Management 
+
+    Provides an API for fetching and managing the date/time using
+    the Dreamcast's real-time clock. All timestamps are in standard
+    Unix format, with an epoch of January 1, 1970. Due to the fact
+    that there is no time zone data on the RTC, all times are expected
+    to be in the local time zone.
+
+    \note
+    The RTC that is used by the DC is located on the AICA rather than SH4, 
+    presumably for power-efficiency reasons. Because of this, accessing 
+    it requires a trip over the G2 BUS, which is notoriously slow.
+
+    \note 
+    For reading the current date/time, you should favor the standard C,
+    C++, or POSIX functions, as they are platform-indpendent and are 
+    calculating current time based on a cached boot time plus a delta
+    that is maintained by the timer subsystem, rather than actually
+    having to requery the RTC over the G2 BUS, so they are faster.
+
+    \warning
+    Internally, the RTC's date/time is maintained using a 32-bit counter 
+    with an epoch of January 1, 1950 00:00. Because of this, the Dreamcast's
+    Y2K and the last timestamp it can represent before rolling over is 
+    February 06 2086 06:28:15. 
+*/
+
+/** \brief   Get the current date/time.
+    \ingroup rtc
 
     This function retrieves the current RTC value as a standard UNIX timestamp
     (with an epoch of January 1, 1970 00:00). This is assumed to be in the
     timezone of the user (as the RTC does not support timezones).
 
     \return                 The current UNIX-style timestamp (local time).
+
+    \sa rtc_set_unix_secs(), rtc_boot_time()
 */
-time_t rtc_unix_secs();
+time_t rtc_unix_secs(void);
 
-/** \brief  Get the time that the sytem was booted.
+/** \brief   Get the current date/time.
+    \ingroup rtc
 
-    This function retrieves the RTC value from when KallistiOS was started. As
+    This function sets the current RTC value as a standard UNIX timestamp
+    (with an epoch of January 1, 1970 00:00). This is assumed to be in the
+    timezone of the user (as the RTC does not support timezones).
+
+    \param time             Unix timestamp to set the current time to
+
+    \return                 0 for success or -1 for failure
+
+    \sa rtc_unix_secs()
+*/
+int rtc_set_unix_secs(time_t time);
+
+/** \brief   Get the time that the sytem was booted.
+    \ingroup rtc
+
+    This function retrieves the cached RTC value from when KallistiOS was started. As
     with rtc_unix_secs(), this is a UNIX-style timestamp in local time.
 
     \return                 The boot time as a UNIX-style timestamp.
+
+    \sa rtc_boot_time()
 */
-time_t rtc_boot_time();
+time_t rtc_boot_time(void);
 
 /* \cond */
-/* Init / Shutdown */
-int rtc_init();
-void rtc_shutdown();
+/* Internally called Init / Shutdown */
+int rtc_init(void);
+void rtc_shutdown(void);
 /* \endcond */
 
 __END_DECLS
