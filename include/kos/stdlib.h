86a241222509dd26b73e2ec91df6114974cdbeea
Falco Girgis <gyrovorbis@gmail.com>
2023-08-14 17:51:45 -0500

    Implemented posix_memalign() addition to stdlib.h
    - created kos/stdlib.h as the KOS stdlib private include/extension
    - modified sys/_types.h to conditionally include kos/stdlib.h when
      including the regular stdlib.h (following the pattern established with
    kos/time.h)
    - added POSIX directory and Makefile
    - added posix_memalign.c with the implementation
    - updated CHANGELOG

---
+++ include/kos/stdlib.h
@@ -0,0 +1,30 @@
+/* KallistiOS ##version##
+
+   kos/time.h
+   Copyright (C) 2023 Falco Girgis
+*/
+
+/* 
+   Add select POSIX extensions to stdlib.h which are not present within Newlib.
+*/ 
+
+#ifndef _STDLIB_H_
+#error "Do not include this file directly. Use <stdlib.h> instead."
+#endif /* !_STDLIB_H_ */
+
+#ifndef __KOS_STDLIB_H
+#define __KOS_STDLIB_H
+
+#if !defined(__STRICT_ANSI__) || (__STDC_VERSION__ >= 201112L) || (__cplusplus >= 201703L)
+
+#include <kos/cdefs.h>
+
+__BEGIN_DECLS
+
+
+extern int posix_memalign(void **memptr, size_t alignment, size_t size);
+
+__END_DECLS
+
+#endif /* !defined(__STRICT_ANSI__) || (__STDC_VERSION__ >= 201112L) || (__cplusplus >= 201703L) */
+#endif /* !__KOS_STDLIB_H */
