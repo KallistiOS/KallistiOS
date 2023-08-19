28b6fb98d7a80c00b2dbbee6d0075c2a3a6c11b0
Falco Girgis <gyrovorbis@gmail.com>
2023-08-18 22:05:36 -0500

    Addressed code review requests
    - Spacing/indentation fixed
    - Move inner switch/case scope variables to main function scope

--- kernel/libc/posix/clock_gettime.c
+++ kernel/libc/posix/clock_gettime.c
@@ -11,57 +11,61 @@
 
 int clock_getres(clockid_t clk_id, struct timespec *ts) {
     switch(clk_id) {
-    case CLOCK_REALTIME:
-    case CLOCK_MONOTONIC:
-    case CLOCK_PROCESS_CPUTIME_ID:
-    case CLOCK_THREAD_CPUTIME_ID:
-        if(!ts) {
-            errno = EFAULT;
+        case CLOCK_REALTIME:
+        case CLOCK_MONOTONIC:
+        case CLOCK_PROCESS_CPUTIME_ID:
+        case CLOCK_THREAD_CPUTIME_ID:
+            if(!ts) {
+                errno = EFAULT;
+                return -1;
+            }
+            ts->tv_sec = 0;
+            ts->tv_nsec = 1000 * 1000;
+            return 0;
+            
+        default:
+            errno = EINVAL;
             return -1;
-        }
-        ts->tv_sec = 0;
-        ts->tv_nsec = 1000 * 1000;
-        return 0;
-    default:
-        errno = EINVAL;
-        return -1;
     }
 }
 
 int clock_gettime(clockid_t clk_id, struct timespec *ts) {
+    uint32_t secs, msecs; 
+
     if(!ts) {
         errno = EFAULT;
         return -1;
     }
 
     switch(clk_id) {
-    case CLOCK_REALTIME:
-        return timespec_get(ts, TIME_UTC) == TIME_UTC ? 0 : -1;
-    case CLOCK_MONOTONIC:
-    case CLOCK_PROCESS_CPUTIME_ID:
-    case CLOCK_THREAD_CPUTIME_ID: {
-        uint32_t secs, msecs;
-        timer_ms_gettime(&secs, &msecs);
-        ts->tv_sec = secs;
-        ts->tv_nsec = msecs * 1000 * 1000;
-        return 0;
-    }
-    default:
-        errno = EINVAL;
-        return -1;
+        case CLOCK_REALTIME:
+            return timespec_get(ts, TIME_UTC) == TIME_UTC ? 0 : -1;
+
+        case CLOCK_MONOTONIC:
+        case CLOCK_PROCESS_CPUTIME_ID:
+        case CLOCK_THREAD_CPUTIME_ID:
+            timer_ms_gettime(&secs, &msecs);
+            ts->tv_sec = secs;
+            ts->tv_nsec = msecs * 1000 * 1000;
+            return 0;
+
+        default:
+            errno = EINVAL;
+            return -1;
     }
 }
 
 int clock_settime(clockid_t clk_id, const struct timespec *ts) {
     switch(clk_id) {
-    case CLOCK_REALTIME:
-        if(!ts) {
-            errno = EFAULT;
+        case CLOCK_REALTIME:
+            if(!ts) {
+                errno = EFAULT;
+                return -1;
+            }
+            return rtc_set_unix_secs(ts->tv_sec);
+
+        default:
+            errno = EINVAL;
             return -1;
-        }
-        return rtc_set_unix_secs(ts->tv_sec);
-    default:
-        errno = EINVAL;
-        return -1;
     }
 }
