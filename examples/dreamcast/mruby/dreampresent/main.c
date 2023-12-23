45cac5d46408de246360a487c37f1e35339620f6
SiZiOUS <sizious@gmail.com>
2023-12-23 15:13:49 +0000

    `dreampresent`: minor changes before committing the sample to KOS

--- examples/dreamcast/mruby/dreampresent/main.c
+++ examples/dreamcast/mruby/dreampresent/main.c
@@ -1,11 +1,31 @@
+/* KallistiOS ##version##
+
+   main.c
+   Copyright (C) 2019-2024 Yuji Yokoo
+
+   A simple presentation tool for Sega Dreamcast written in Ruby
+*/
+
 #include <kos.h>
 #include <mruby/mruby.h>
 #include <mruby/mruby/irep.h>
 #include "dckos.h"
 
+/* These macros tell KOS how to initialize itself. All of this initialization
+   happens before main() gets called, and the shutdown happens afterwards. So
+   you need to set any flags you want here. Here are some possibilities:
+
+   INIT_NONE         -- don't do any auto init
+   INIT_IRQ          -- knable IRQs
+   INIT_THD_PREEMPT  -- Enable pre-emptive threading
+   INIT_NET          -- Enable networking (doesn't imply lwIP!)
+   INIT_MALLOCSTATS  -- Enable a call to malloc_stats() right before shutdown
+
+   You can OR any or all of those together. If you want to start out with
+   the current KOS defaults, use INIT_DEFAULT (or leave it out entirely). */
 KOS_INIT_FLAGS(INIT_DEFAULT | INIT_MALLOCSTATS);
 
-/* Compiled ruby code */
+/* Compiled Ruby code, declared in the RB file */
 extern const uint8_t dreampresent_bytecode[];
 
 int main(int argc, char **argv) {
