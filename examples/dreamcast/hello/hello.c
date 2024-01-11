21e4bf81efc2d96f862f52c1584d1177c9e136c3
Falco Girgis <gyrovorbis@gmail.com>
2024-01-10 23:11:00 -0600

    Cleaned up and updated hello example
    - Removed totally unneeded romdisk which wasn't even used
    - Added more comments and descriptions to the Makefile for each rule
    - Commented out explicit KOS_INIT_FLAGS() to show how defaults can be
      used without including a single KOS header
    - Added more details about how KOS_INIT_FLAGS() work.

--- examples/dreamcast/hello/hello.c
+++ examples/dreamcast/hello/hello.c
@@ -2,25 +2,38 @@
 
    hello.c
    Copyright (C) 2001 Megan Potter
+   Copyright (C) 2024 Falco Girgis
 */
 
-#include <kos.h>
+#include <stdio.h>
 
-/* These macros tell KOS how to initialize itself. All of this initialization
-   happens before main() gets called, and the shutdown happens afterwards. So
-   you need to set any flags you want here. Here are some possibilities:
+/* #include <kos.h>
+   KOS_INIT_FLAGS(INIT_DEFAULT);
+*/
+
+/* KOS_INIT_FLAGS() tells KOS how to initialize itself and which drivers to
+   pull into your executable. All of this initialization happens before main()
+   gets called, and the shutdown happens afterwards.
+
+   Here are some possible flags to pass to KOS_INIT_FLAGS():
 
    INIT_NONE        -- don't do any auto init
    INIT_IRQ         -- Enable IRQs
    INIT_NET         -- Enable networking (including sockets)
    INIT_MALLOCSTATS -- Enable a call to malloc_stats() right before shutdown
 
-   You can OR any or all of those together. If you want to start out with
-   the current KOS defaults, use INIT_DEFAULT (or leave it out entirely). */
-KOS_INIT_FLAGS(INIT_DEFAULT | INIT_MALLOCSTATS);
+   Refer to kos/init.h and arch/init_flags.h for the full list of flags.
+   You can OR any or all of these together.
+
+   If you want to start out with the current KOS defaults, use INIT_DEFAULT.
+   You may now also omit KOS_INIT_FLAGS() altogether, which will also use
+   INIT_DEFAULT automatically, as we're doing here. This can be useful if
+   you're lazy or if you're building regular C or C++ code and don't want to
+   introduce KOS-specific DC code into the file.
+*/
 
 /* Your program's main entry point */
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     /* The requisite line */
     printf("\nHello world!\n\n");
 
