47b0249154f5a9bc5acc446bdc15777e6adb3dc4
Falco Girgis <gyrovorbis@gmail.com>
2024-01-28 21:31:07 -0600

    Addressing review feedback.

--- kernel/libc/newlib/newlib_kill.c
+++ kernel/libc/newlib/newlib_kill.c
@@ -2,26 +2,15 @@
 
    newlib_kill.c
    Copyright (C) 2004 Megan Potter
-   Copyright (C) 2024 Falco Girgis
 
 */
 
-#include <kos/thread.h>
 #include <sys/reent.h>
 #include <errno.h>
 
 int _kill_r(struct _reent *reent, int pid, int sig) {
-    if(pid <= 0 || pid == KOS_PID) {
-        /* NULL signals are simply ignored, while any other
-           signal will terminate the application with the signal
-           value as its return value.
-         */
-        if(sig)
-            exit(sig);
-
-        return 0;
-    }
-
-    reent->_errno = ESRCH;
+    (void)pid;
+    (void)sig;
+    reent->_errno = EINVAL;
     return -1;
 }
