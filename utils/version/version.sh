aa0fb900f93e968d72f46d915245e683a5c5995e
Falco Girgis <gyrovorbis@gmail.com>
2024-06-08 15:41:20 -0500

    Got KOS's versioning working correctly!!!
    - Added version environment variables to environ_base.sh, which get
      extracted from include/kos/version.h as the main version source.
    - Added version information to KOS banner via make_banner.sh.
    - Added automatic version extraction to version.sh utility when
      the command-line arg doesn't explicitly provide a version.

--- utils/version/version.sh
+++ utils/version/version.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
 # Little version release util for KOS
-# (c)2000-2002 Megan Potter
+# Copyright (C) 2000-2002 Megan Potter
+# Copyright (C) 2024 Falco Girgis
 
 # Call this program on each file to substitute in the proper version code
 # for the version header. This works with find|xargs.
+#
+# NOTE: Typically you do not want to explicitly provide a version number
+#       to this script, as it will automatically source the version
+# 		information from the KOS environment variables which come from
+#       include/kos/version.h, which should always be the main source of
+#       truth for a release version!
 
 VERSION=$1
+if [[ -z "$var" ]]; then
+	VERSION="$KOS_VERSION"
+fi
 shift
 for i in $*; do
 	echo processing $i to version $VERSION
