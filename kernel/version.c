4ba093e2b73a3eb1328ab43b5ea0bc6ac1b3f3fb
Falco Girgis <gyrovorbis@gmail.com>
2024-06-08 04:31:02 -0500

    Finished up new versioning scheme.
    - Created both compile-time and run-time version identifiers and
      requirement checks.
    - Exposed a bunch of inner macros as public utilities for apps to use
      to add their own versioning scheme.
    - Documented the living crap out of everything.
    - Still need to add the version to the banner.

--- kernel/version.c
+++ kernel/version.c
@@ -7,3 +7,15 @@ kos_version_t kos_version(void) {
 const char* kos_version_string(void) {
     return KOS_VERSION_STRING;
 }
+
+bool kos_version_min(uint8_t major, uint16_t minor, uint8_t patch) {
+    return KOS_VERSION_MIN(major, minor, patch);
+}
+
+bool kos_version_equal(uint8_t major, uint16_t minor, uint8_t patch) {
+    return KOS_VERSION_EQUAL(major, minor, patch);
+}
+
+bool kos_version_max(uint8_t major, uint16_t minor, uint8_t patch) {
+    return KOS_VERSION_MAX(major, minor, patch);
+}
