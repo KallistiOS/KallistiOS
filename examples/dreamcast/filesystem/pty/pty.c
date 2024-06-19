e91ea9c13d9f7f9e3c66a36d21a1d81696b9cc64
Andress Barajas <andress.barajas@gmail.com>
2024-06-18 21:34:21 -0700

    Added pty example to fix issue #224

---
+++ examples/dreamcast/filesystem/pty/pty.c
@@ -0,0 +1,53 @@
+/* KallistiOS ##version##
+
+   pty.c
+   Copyright (C) 2024 Andress Barajas
+*/
+
+#include <kos/fs_pty.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+
+    /* Create a PTY pair */
+    file_t master_fd, slave_fd;
+    if (fs_pty_create(NULL, 0, &master_fd, &slave_fd) < 0) {
+        perror("Error creating PTY pair");
+        fs_pty_shutdown();
+        return 1;
+    }
+
+    /* Write to the master end of the PTY */
+    const char *msg = "Hello from master!";
+    if (write(master_fd, msg, strlen(msg)) < 0) {
+        perror("Error writing to master PTY");
+        fs_close(master_fd);
+        fs_close(slave_fd);
+        fs_pty_shutdown();
+        return 1;
+    }
+
+    /* Read from the slave end of the PTY */
+    char buffer[128];
+    ssize_t bytes_read = read(slave_fd, buffer, sizeof(buffer) - 1);
+    if (bytes_read < 0) {
+        perror("Error reading from slave PTY");
+        fs_close(master_fd);
+        fs_close(slave_fd);
+        fs_pty_shutdown();
+        return 1;
+    }
+
+    /* Null-terminate and print the received message */
+    buffer[bytes_read] = '\0';
+    printf("Received message: %s\n", buffer);
+
+    /* Clean up resources */
+    fs_close(master_fd);
+    fs_close(slave_fd);
+
+    return 0;
+}
