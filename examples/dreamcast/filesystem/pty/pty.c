52202ed3fb00961a7241bc916d142e9dde6d00f2
Andress Barajas <andress.barajas@gmail.com>
2024-06-18 21:39:46 -0700

    More style fixes

--- examples/dreamcast/filesystem/pty/pty.c
+++ examples/dreamcast/filesystem/pty/pty.c
@@ -15,18 +15,16 @@ int main(int argc, char* argv[]) {
     /* Create a PTY pair */
     file_t master_fd, slave_fd;
     if (fs_pty_create(NULL, 0, &master_fd, &slave_fd) < 0) {
-        perror("Error creating PTY pair");
-        fs_pty_shutdown();
+        fprintf(stderr, "Error creating PTY pair");
         return 1;
     }
 
     /* Write to the master end of the PTY */
     const char *msg = "Hello from master!";
     if (write(master_fd, msg, strlen(msg)) < 0) {
-        perror("Error writing to master PTY");
+        fprintf(stderr, "Error writing to master PTY");
         fs_close(master_fd);
         fs_close(slave_fd);
-        fs_pty_shutdown();
         return 1;
     }
 
@@ -34,10 +32,9 @@ int main(int argc, char* argv[]) {
     char buffer[128];
     ssize_t bytes_read = read(slave_fd, buffer, sizeof(buffer) - 1);
     if (bytes_read < 0) {
-        perror("Error reading from slave PTY");
+        fprintf(stderr, "Error reading from slave PTY");
         fs_close(master_fd);
         fs_close(slave_fd);
-        fs_pty_shutdown();
         return 1;
     }
 
