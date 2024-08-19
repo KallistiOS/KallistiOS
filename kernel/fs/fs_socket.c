cc5dc1df7fcbbd043e8528b3c9d230ca880bc7be
Andress Barajas <andress.barajas@gmail.com>
2024-08-18 17:57:36 -0700

    Add getpeername
    Added getpeername to sockets to help with cURL kos-port

--- kernel/fs/fs_socket.c
+++ kernel/fs/fs_socket.c
@@ -542,6 +542,25 @@ int getsockname(int sock, struct sockaddr *name, socklen_t *name_len) {
     return hnd->protocol->getsockname(hnd, name, name_len);
 }
 
+int getpeername(int sock, struct sockaddr *restrict name, socklen_t *restrict name_len) {
+    net_socket_t *hnd;
+
+    hnd = (net_socket_t *)fs_get_handle(sock);
+
+    if(hnd == NULL) {
+        errno = EBADF;
+        return -1;
+    }
+
+    /* Make sure this is actually a socket. */
+    if(fs_get_handler(sock) != &vh) {
+        errno = ENOTSOCK;
+        return -1;
+    }
+
+    return hnd->protocol->getpeername(hnd, name, name_len);
+}
+
 int getsockopt(int sock, int level, int option_name, void *option_value,
                socklen_t *option_len) {
     net_socket_t *hnd;
