KallistiOS/KallistiOS

commit 2c2e3d2606d2e844e936a41e4d780678a9f76504

Validate inputs, OOM, and cleanup in vmu_pkg.

kernel/arch/dreamcast/util/vmu_pkg.c
@@ -53,21 +53,22 @@ struct bmp_dib_header {
 };
 
 /* CRC calculation: calculates the CRC on a VMU file to be written out */
-static int vmu_pkg_crc(const uint8 * buf, int size) {
-    int i, c, n;
+static uint16_t vmu_pkg_crc(const uint8_t *buf, size_t size) {
+    size_t i;
+    uint16_t c, n;
 
     for(i = 0, n = 0; i < size; i++) {
         n ^= (buf[i] << 8);
 
         for(c = 0; c < 8; c++) {
             if(n & 0x8000)
-                n = (n << 1) ^ 4129;
+                n = (n << 1) ^ 0x1021;
             else
                 n = (n << 1);
         }
     }
 
-    return n & 0xffff;
+    return n;
 }
 
 static int vmu_eyecatch_size(int eyecatch_type) {
@@ -85,12 +86,14 @@ static int vmu_eyecatch_size(int eyecatch_type) {
     }
 }
 
-/* Converts a vmu_pkg_t structure into an array of uint8's which may be
+/* Converts a vmu_pkg_t structure into an array of uint8_t's which may be
    written to a VMU file via fs_vmu, or whatever. */
-int vmu_pkg_build(vmu_pkg_t *src, uint8 ** dst, int * dst_size) {
-    uint8       *out;
-    int     ec_size, out_size;
+int vmu_pkg_build(vmu_pkg_t *src, uint8_t **dst, int *dst_size) {
+    uint8_t     *out;
+    int         ec_size, out_size;
     vmu_hdr_t   *hdr;
+
+    assert(src && dst);
 
     /* First off, figure out how big it will be */
     out_size = sizeof(vmu_hdr_t) + 512 * src->icon_cnt + src->data_len;
@@ -99,10 +102,11 @@ int vmu_pkg_build(vmu_pkg_t *src, uint8 ** dst, int * dst_size) {
     if(ec_size < 0) return -1;
 
     out_size += ec_size;
-    *dst_size = out_size;
 
     /* Allocate a return array */
-    out = *dst = malloc(out_size);
+    out = malloc(out_size);
+
+    if(!out) return -1;
 
     /* Setup some defaults */
     memset(out, 0, out_size);
@@ -138,15 +142,21 @@ int vmu_pkg_build(vmu_pkg_t *src, uint8 ** dst, int * dst_size) {
     /* Calculate CRC */
     hdr->crc = vmu_pkg_crc(out, out_size);
 
+    /* We're done so write outputs */
+    *dst_size = out_size;
+    *dst = out;
+
     return 0;
 }
 
-/* Parse an array of uint8's (i.e. a VMU data file) into a
+/* Parse an array of uint8_t's (i.e. a VMU data file) into a
  * vmu_pkg_t package structure. */
-int vmu_pkg_parse(uint8 *data, vmu_pkg_t *pkg) {
-    uint16 crc, crc_save;
+int vmu_pkg_parse(uint8_t *data, vmu_pkg_t *pkg) {
+    uint16_t crc, crc_save;
     int ec_size, hdr_size, total_size, icon_size;
     vmu_hdr_t *hdr;
+
+    assert(data && pkg);
 
     hdr = (vmu_hdr_t *)data;
 

kernel/arch/dreamcast/include/dc/vmu_pkg.h
@@ -23,7 +23,7 @@
 #include <sys/cdefs.h>
 __BEGIN_DECLS
 
-#include <arch/types.h>
+#include <stdint.h>
 
 /** \defgroup   vmu_package     Header Package
     \brief                      API for Managing VMU File Headers
@@ -44,17 +44,17 @@ __BEGIN_DECLS
     \headerfile dc/vmu_pkg.h
 */
 typedef struct vmu_pkg {
-    char        desc_short[20];     /**< \brief Short file description */
-    char        desc_long[36];      /**< \brief Long file description */
-    char        app_id[20];         /**< \brief Application ID */
-    int         icon_cnt;           /**< \brief Number of icons */
-    int         icon_anim_speed;    /**< \brief Icon animation speed */
-    int         eyecatch_type;      /**< \brief "Eyecatch" type */
-    int         data_len;           /**< \brief Number of data (payload) bytes */
-    uint16      icon_pal[16];       /**< \brief Icon palette (ARGB4444) */
-    uint8       *icon_data;         /**< \brief 512*n bytes of icon data */
-    const uint8 *eyecatch_data;     /**< \brief Eyecatch data */
-    const uint8 *data;              /**< \brief Payload data */
+    char            desc_short[20];     /**< \brief Short file description */
+    char            desc_long[36];      /**< \brief Long file description */
+    char            app_id[20];         /**< \brief Application ID */
+    int             icon_cnt;           /**< \brief Number of icons */
+    int             icon_anim_speed;    /**< \brief Icon animation speed */
+    int             eyecatch_type;      /**< \brief "Eyecatch" type */
+    int             data_len;           /**< \brief Number of data (payload) bytes */
+    uint16_t        icon_pal[16];       /**< \brief Icon palette (ARGB4444) */
+    uint8_t         *icon_data;         /**< \brief 512*n bytes of icon data */
+    const uint8_t   *eyecatch_data;     /**< \brief Eyecatch data */
+    const uint8_t   *data;              /**< \brief Payload data */
 } vmu_pkg_t;
 
 /** \brief   Final VMU package type.
@@ -65,16 +65,16 @@ typedef struct vmu_pkg {
     \headerfile dc/vmu_pkg.h
 */
 typedef struct vmu_hdr {
-    char    desc_short[16];     /**< \brief Space-padded short description */
-    char    desc_long[32];      /**< \brief Space-padded long description*/
-    char    app_id[16];         /**< \brief Null-padded application ID */
-    uint16  icon_cnt;           /**< \brief Number of icons */
-    uint16  icon_anim_speed;    /**< \brief Icon animation speed */
-    uint16  eyecatch_type;      /**< \brief Eyecatch type */
-    uint16  crc;                /**< \brief CRC of the file */
-    uint32  data_len;           /**< \brief Payload size */
-    uint8   reserved[20];       /**< \brief Reserved (all zero) */
-    uint16  icon_pal[16];       /**< \brief Icon palette (ARGB4444) */
+    char        desc_short[16];     /**< \brief Space-padded short description */
+    char        desc_long[32];      /**< \brief Space-padded long description*/
+    char        app_id[16];         /**< \brief Null-padded application ID */
+    uint16_t    icon_cnt;           /**< \brief Number of icons */
+    uint16_t    icon_anim_speed;    /**< \brief Icon animation speed */
+    uint16_t    eyecatch_type;      /**< \brief Eyecatch type */
+    uint16_t    crc;                /**< \brief CRC of the file */
+    uint32_t    data_len;           /**< \brief Payload size */
+    uint8_t     reserved[20];       /**< \brief Reserved (all zero) */
+    uint16_t    icon_pal[16];       /**< \brief Icon palette (ARGB4444) */
     /* 512*n Icon Bitmaps */
     /* Eyecatch palette + bitmap */
 } vmu_hdr_t;
@@ -106,7 +106,7 @@ typedef struct vmu_hdr {
     \param  dst_size        The size of the output.
     \return                 0 on success, <0 on failure.
 */
-int vmu_pkg_build(vmu_pkg_t *src, uint8 ** dst, int * dst_size);
+int vmu_pkg_build(vmu_pkg_t *src, uint8_t ** dst, int * dst_size);
 
 /** \brief   Parse an array of uint8s into a vmu_pkg_t.
     \ingroup vmu_package
@@ -119,7 +119,7 @@ int vmu_pkg_build(vmu_pkg_t *src, uint8 ** dst, int * dst_size);
     \retval -1              On invalid CRC in the data.
     \retval 0               On success.
 */
-int vmu_pkg_parse(uint8 *data, vmu_pkg_t *pkg);
+int vmu_pkg_parse(uint8_t *data, vmu_pkg_t *pkg);
 
 /** \brief   Load a .ico file to use as a VMU file's icon.
     \ingroup vmu_package
@@ -148,4 +148,3 @@ int vmu_pkg_load_icon(vmu_pkg_t *pkg, const char *icon_fn);
 __END_DECLS
 
 #endif  /* __DC_VMU_PKG_H */
-

commit 1acf10340962fbd6b7fe45dd203fa42d94f77008

Correct erroneous resetting of output buffer in vmu_pkg_build.
Regression from #907

kernel/arch/dreamcast/util/vmu_pkg.c
@@ -108,6 +108,10 @@ int vmu_pkg_build(vmu_pkg_t *src, uint8_t **dst, int *dst_size) {
 
     if(!out) return -1;
 
+    /* We're done so write outputs */
+    *dst_size = out_size;
+    *dst = out;
+
     /* Setup some defaults */
     memset(out, 0, out_size);
     hdr = (vmu_hdr_t *)out;
@@ -141,10 +145,6 @@ int vmu_pkg_build(vmu_pkg_t *src, uint8_t **dst, int *dst_size) {
 
     /* Calculate CRC */
     hdr->crc = vmu_pkg_crc(out, out_size);
-
-    /* We're done so write outputs */
-    *dst_size = out_size;
-    *dst = out;
 
     return 0;
 }