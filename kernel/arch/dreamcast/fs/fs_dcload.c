eed909096a60b98fb8b1560e08e94266c3097523
QuzarDC <quzar@completext.com>
2025-05-11 01:50:52 -0400

    Rename and expose if the dcload syscall is installed.
    Removed two usages that were redundant. There is no
    condition where `dcload_type` could be set to `TYPE_SER`
    while dcload is not detected.
    
    Renamed to indicate that it's not a part of the dcload vfs.

--- kernel/arch/dreamcast/fs/fs_dcload.c
+++ kernel/arch/dreamcast/fs/fs_dcload.c
@@ -485,12 +485,12 @@ dbgio_handler_t dbgio_dcload = {
     NULL
 };
 
-int fs_dcload_detected(void) {
+bool syscall_dcload_detected(void) {
     /* Check for dcload */
     if(*DCLOADMAGICADDR == DCLOADMAGICVALUE)
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
 
 static int *dcload_wrkmem = NULL;
@@ -503,13 +503,13 @@ void fs_dcload_init_console(void) {
     /* Setup our dbgio handler */
     memcpy(&dbgio_dcload, &dbgio_null, sizeof(dbgio_dcload));
     dbgio_dcload.name = dbgio_dcload_name;
-    dbgio_dcload.detected = fs_dcload_detected;
+    dbgio_dcload.detected = syscall_dcload_detected;
     dbgio_dcload.write_buffer = dcload_write_buffer;
     // dbgio_dcload.read = dcload_read_cons;
 
-    /* We actually need to detect here to make sure we're not on
+    /* We actually need to detect here to make sure we're on
        dcload-serial, or scif_init must not proceed. */
-    if(*DCLOADMAGICADDR != DCLOADMAGICVALUE)
+    if(!syscall_dcload_detected())
         return;
 
 
@@ -548,7 +548,7 @@ void fs_dcload_init(void) {
 
 void fs_dcload_shutdown(void) {
     /* Check for dcload */
-    if(*DCLOADMAGICADDR != DCLOADMAGICVALUE)
+    if(!syscall_dcload_detected())
         return;
 
     /* Free dcload wrkram */
