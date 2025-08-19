a35aec877cbc14129750ecf2cdd698f06835eb32
Luna <cli-luna@protonmail.com>
2025-08-19 17:32:57 +0000

    Add suggested changes

--- utils/dc-chain/scripts/gcc-pass2.mk
+++ utils/dc-chain/scripts/gcc-pass2.mk
@@ -31,6 +31,14 @@ ifdef enable_ada
 	$(MAKE) $(jobs_arg) -C $(build)/gcc cross-gnattools ada.all.cross DESTDIR=$(DESTDIR) $(to_log)
   endif
 endif
+ifdef enable_d
+  ifneq (0,$(enable_d))
+	@echo "+++ Running post-install fixups for GNU D..."
+	-mkdir -p $(sh_toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d
+	cp -r ./gcc-$(gcc_ver)/libphobos/libdruntime/* $(sh_toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d
+	cp $(patches)/gdc/config.d $(sh_toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d/gcc/config.d
+  endif
+endif
 	$(MAKE) -C $(build) $(install_mode) DESTDIR=$(DESTDIR) $(to_log)
 	$(target)-gcc-ar d $(shell $(target)-gcc -print-file-name=libgcc.a) fake-kos.o
 	$(clean_up)
