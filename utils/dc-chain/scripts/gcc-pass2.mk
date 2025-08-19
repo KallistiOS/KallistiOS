c7b33e7fc79866e98a15e64b65f142c83d0fe0d9
Luna <cli-luna@protonmail.com>
2025-08-19 20:48:15 +0200

    sh_toolchain_path -> toolchain_path

--- utils/dc-chain/scripts/gcc-pass2.mk
+++ utils/dc-chain/scripts/gcc-pass2.mk
@@ -34,9 +34,9 @@ endif
 ifdef enable_d
   ifneq (0,$(enable_d))
 	@echo "+++ Running post-install fixups for GNU D..."
-	-mkdir -p $(sh_toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d
+	-mkdir -p $(toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d
 	cp -r ./gcc-$(gcc_ver)/libphobos/libdruntime/* $(sh_toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d
-	cp $(patches)/gdc/config.d $(sh_toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d/gcc/config.d
+	cp $(patches)/gdc/config.d $(toolchain_path)/lib/gcc/$(target)/$(gcc_ver)/include/d/gcc/config.d
   endif
 endif
 	$(MAKE) -C $(build) $(install_mode) DESTDIR=$(DESTDIR) $(to_log)
