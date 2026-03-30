1b8ca8868448b51662aaefd12a8b8631df78eda2
Paul Cercueil <paul@crapouillou.net>
2026-03-30 15:08:56 +0200

    dbglog: Simplify logging facility
    Instead of calling vsnprintf() to process the format string + arguments
    into a non-reentrant static buffer, that then needs to be protected
    against concurrent accesses, just rely on the fact that the C library
    already provides such a mechanism.
    
    Update dbglog() to a simple macro that will basically call printf() for
    standard messages, below the warning threshold, and fprintf(stderr) for
    the important messages.
    
    The macro will also check that the requested log level is below the one
    set to KallistiOS at compilation time, and the one dynamically set with
    dbglog_set_level().
    
    Note the use of __builtin_choose_expr(), this is to allow using printf()
    / fprintf(stderr) instead of fprintf(stdout) / fprintf(stderr).
    The distinction is important, as the compiler will change the calls to
    printf() with static strings with no format arguments to calls to
    puts(). On the ARM port of KallistiOS, these strings can then be
    transmitted to the SH4 side much more efficiently and asynchronously.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/libc/koslib/dbglog.c
+++ kernel/libc/koslib/dbglog.c
@@ -1,23 +1,14 @@
 /* KallistiOS ##version##
 
    dbglog.c
-   Copyright (C)2000,2001,2004 Megan Potter
+   Copyright (C) 2000,2001,2004 Megan Potter
+   Copyright (C) 2026 Paul Cercueil
 */
 
 #include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
 
 #include <kos/dbglog.h>
-#include <kos/thread.h>
-#include <kos/dbgio.h>
-#include <kos/fs.h>
-#include <kos/spinlock.h>
-
-/* Not re-entrant */
-static char printf_buf[1024];
-static spinlock_t mutex = SPINLOCK_INITIALIZER;
 
 /* Default kernel debug log level: if a message has a level higher than this,
    it won't be shown. Set to DBG_DEAD to see basically nothing, and set to
@@ -27,31 +18,4 @@ int dbglog_level = (DBGLOG_LEVEL_SUPPORT < DBG_INFO) ? DBGLOG_LEVEL_SUPPORT : DB
 /* Set debug level */
 void dbglog_set_level(int level) {
     dbglog_level = level;
-}
-
-/* Kernel debug logging facility */
-void __real_dbglog(int level, const char *fmt, ...) {
-    va_list args;
-    int i;
-
-    /* If this log level is blocked out, don't even bother */
-    if((DBGLOG_LEVEL_SUPPORT < level) || (level > dbglog_level))
-        return;
-
-    /* We only try to lock if the message isn't urgent */
-    if(level >= DBG_ERROR && !irq_inside_int())
-        spinlock_lock(&mutex);
-
-    va_start(args, fmt);
-    i = vsnprintf(printf_buf, sizeof(printf_buf), fmt, args);
-    va_end(args);
-
-    if(i > 0) {
-        if(irq_inside_int() || 
-            (fs_write(STDOUT_FILENO, printf_buf, strlen(printf_buf)) < 0))
-                dbgio_write_str(printf_buf);
-    }
-
-    if(level >= DBG_ERROR && !irq_inside_int())
-        spinlock_unlock(&mutex);
 }
