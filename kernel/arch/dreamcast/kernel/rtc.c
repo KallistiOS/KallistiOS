a6b009e4cc92a96deb36bc9bbffcf758d2404cd4
Paul Cercueil <paul@crapouillou.net>
2026-04-01 11:59:04 +0200

    rtc: Whitespace fixes
    No functional change.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/arch/dreamcast/kernel/rtc.c
+++ kernel/arch/dreamcast/kernel/rtc.c
@@ -114,7 +114,7 @@ time_t rtc_unix_secs(void) {
     return rtcnew - RTC_UNIX_EPOCH_DELTA;
 }
 
-/* Sets the date/time value from a UNIX epoch time stamp, 
+/* Sets the date/time value from a UNIX epoch time stamp,
    returning 0 for success or -1 for failure. */
 int rtc_set_unix_secs(time_t secs) {
     int result = 0;
@@ -135,10 +135,10 @@ int rtc_set_unix_secs(time_t secs) {
     /* Enable writing by setting LSB of control */
     g2_write_32(RTC_CTRL_ADDR, RTC_CTRL_WRITE_EN);
 
-    /* Try 3 times to ensure we didn't write a value then have 
+    /* Try 3 times to ensure we didn't write a value then have
        the clock increment itself before the next. */
     for(i = 0; i < RTC_RETRY_COUNT; i++) {
-        /* Write the least-significant 16-bits first, because 
+        /* Write the least-significant 16-bits first, because
            writing to the high 16-bits will lock RTC writes. */
         g2_write_32(RTC_TIMESTAMP_LOW_ADDR, (adjusted) & 0xffff);
         g2_write_32(RTC_TIMESTAMP_HIGH_ADDR, (adjusted >> 16) & 0xffff);
@@ -161,7 +161,7 @@ int rtc_set_unix_secs(time_t secs) {
     }
 
     /* We have to update the boot time now as well, subtracting
-       the amount of time that has elapsed since boot from the 
+       the amount of time that has elapsed since boot from the
        new time we've just set. */
     timer_ms_gettime(&s, &ms);
     boot_time = ((time_t)rtcnew - RTC_UNIX_EPOCH_DELTA) - s;
