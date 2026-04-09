0b96315bb4a955474fd4983c6545c5b648b6fbd6
Andress Barajas <andress.barajas@gmail.com>
2026-04-09 14:55:04 -0700

    gdb: add Z0 software breakpoints

--- kernel/arch/dreamcast/kernel/gdb/gdb_break.c
+++ kernel/arch/dreamcast/kernel/gdb/gdb_break.c
@@ -8,6 +8,11 @@
 
 */
 
+#include <arch/arch.h>
+#include <arch/cache.h>
+
+#include <dc/memory.h>
+
 #include "gdb_internal.h"
 
 /* Handle inserting/removing a hardware breakpoint.
@@ -19,6 +24,98 @@
 #define LREG(r, o) (*((uint32_t*)((r)+(o))))
 #define WREG(r, o) (*((uint16_t*)((r)+(o))))
 #define BREG(r, o) (*((uint8_t*)((r)+(o))))
+
+#define MAX_SW_BREAKPOINTS 32
+#define GDB_SW_BREAK_OPCODE 0xc33f
+
+typedef struct {
+    uintptr_t addr;
+    uint16_t original;
+    bool active;
+} sw_breakpoint_t;
+
+static sw_breakpoint_t sw_breakpoints[MAX_SW_BREAKPOINTS];
+
+static uintptr_t normalize_cached_address(uintptr_t addr) {
+    return (addr & MEM_AREA_CACHE_MASK) | MEM_AREA_P1_BASE;
+}
+
+static bool is_valid_sw_breakpoint_range(uintptr_t addr, size_t length) {
+    uintptr_t end_addr;
+
+    if(length == 0)
+        return false;
+
+    end_addr = addr + length - 1u;
+    if(end_addr < addr)
+        return false;
+
+    return arch_valid_text_address(normalize_cached_address(addr)) &&
+           arch_valid_text_address(normalize_cached_address(end_addr));
+}
+
+static void soft_breakpoint(bool set, uintptr_t addr, size_t length) {
+    uintptr_t normalized_addr;
+    volatile uint16_t *site;
+
+    if((addr & 1u) || length != 2u) {
+        gdb_error_with_code_str(GDB_EINVAL,
+                                "Z0: address must be 2-byte aligned");
+        return;
+    }
+
+    if(!is_valid_sw_breakpoint_range(addr, length)) {
+        gdb_error_with_code_str(GDB_EMEM_PROT,
+                                "Z0: invalid software breakpoint address");
+        return;
+    }
+
+    normalized_addr = normalize_cached_address(addr);
+    site = (volatile uint16_t *)normalized_addr;
+
+    for(int i = 0; i < MAX_SW_BREAKPOINTS; ++i) {
+        if(sw_breakpoints[i].active &&
+           sw_breakpoints[i].addr == normalized_addr) {
+            if(set) {
+                gdb_put_ok();
+            }
+            else {
+                if(*site != GDB_SW_BREAK_OPCODE) {
+                    gdb_error_with_code_str(GDB_EBKPT_CLEAR_ID,
+                                            "z0: breakpoint site changed while active");
+                    return;
+                }
+
+                *site = sw_breakpoints[i].original;
+                icache_flush_range(normalized_addr, 2);
+                sw_breakpoints[i].active = false;
+                gdb_put_ok();
+            }
+
+            return;
+        }
+    }
+
+    if(!set) {
+        gdb_error_with_code_str(GDB_EBKPT_CLEAR_ADDR,
+                                "z0: no breakpoint at requested address");
+        return;
+    }
+
+    for(int i = 0; i < MAX_SW_BREAKPOINTS; ++i) {
+        if(!sw_breakpoints[i].active) {
+            sw_breakpoints[i].addr = normalized_addr;
+            sw_breakpoints[i].original = *site;
+            sw_breakpoints[i].active = true;
+            *site = GDB_SW_BREAK_OPCODE;
+            icache_flush_range(normalized_addr, 2);
+            gdb_put_ok();
+            return;
+        }
+    }
+
+    gdb_error_with_code_str(GDB_EBKPT_SW_NORES, "Z0: no free breakpoint slots");
+}
 
 static void hard_breakpoint(bool set, int brk_type, uintptr_t addr, size_t length, char* res_buffer) {
     char* const ucb_base = (char*)0xff200000;
@@ -109,8 +206,12 @@ void handle_breakpoint(char *ptr) {
 
     if(*ptr++ == ',' && hex_to_int(&ptr, &addr) &&
        *ptr++ == ',' && hex_to_int(&ptr, &length) && *ptr == '\0') {
-        hard_breakpoint(set, brk_type, addr, length, remcom_out_buffer);
-    } else {
-        strcpy(remcom_out_buffer, "E02");
+        if(brk_type == 0)
+            soft_breakpoint(set, addr, length);
+        else
+            hard_breakpoint(set, brk_type, addr, length, remcom_out_buffer);
+    }
+    else {
+        gdb_error_with_code_str(GDB_EINVAL, "Z/z: invalid packet");
     }
 }
