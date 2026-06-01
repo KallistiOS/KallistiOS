ea3f74d906cdf66180e74391a3f778eefc78b27f
GPF <troy_ed@yahoo.com>
2026-06-01 00:46:19 -0700

    pvr_mem: Remove redundant initialization of pvr_mem_sbrk in pvr_mem_initialize

--- kernel/arch/dreamcast/hardware/pvr/pvr_mem.c
+++ kernel/arch/dreamcast/hardware/pvr/pvr_mem.c
@@ -198,7 +198,6 @@ void __weak_symbol pvr_mem_reset(void) {
 void __weak_symbol pvr_mem_initialize(pvr_ptr_t pvr_texture_base, size_t available_memory) {
     (void)available_memory;
     pvr_mem_base = pvr_texture_base;
-    pvr_mem_sbrk = pvr_texture_base;
 }
 
 /* Print some statistics (like mallocstats) */
