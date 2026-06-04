3ac7a279652c781fe6d78e2e483e7a7f9cc1ba4e
QuzarDC <quzar@completext.com>
2026-06-04 11:16:18 -0400

    libkmg: Correct loadable module functionality.
    I'm sure this could use extra work and an example to be more usable,
    but these changes bring the module version generally in line with
    our modern usage of the system.
    
    The makefile had added exports.o to the objs which isn't needed as
    well as a libloadable which we don't have.
    
    The exports were unnecessarily exporting the required lib_ functions
    while they were also not being provided.
    
    And finally, updated the gitignore to avoid committing the generated
    exports.c.

--- addons/libkmg/module_main.c
+++ addons/libkmg/module_main.c
@@ -7,7 +7,23 @@
 
 #include <kos/library.h>
 #include <kos/exports.h>
+#include <kos/dbglog.h>
+#include <kmg/kmg.h>
 
 const char *lib_get_name(void) {
     return "imgloader.kmg";
+}
+
+const uint32_t lib_get_version(void) {
+    return KMG_VERSION;
+}
+
+int lib_open(klibrary_t *lib) {
+    dbglog(DBG_INFO, "Library \"%s\" opened.\n", lib_get_name());
+    return 0;
+}
+
+int lib_close(klibrary_t *lib) {
+    dbglog(DBG_INFO, "Library \"%s\" closed.\n", lib_get_name());
+    return 0;
 }
