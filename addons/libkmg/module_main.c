61545f2294a72e3f1d6e534d9fc6e1a7955d9901
QuzarDC <quzar@completext.com>
2026-06-03 22:05:50 -0400

    addons: Migrate libkmg into addons from kos-ports
    Minor changes to the makefile and one fixup to clear
    a warning on signedness.

---
+++ addons/libkmg/module_main.c
@@ -0,0 +1,13 @@
+/* KallistiOS ##version##
+
+   module_main.c
+   Copyright (C)2003 Megan Potter
+
+*/
+
+#include <kos/library.h>
+#include <kos/exports.h>
+
+const char *lib_get_name(void) {
+    return "imgloader.kmg";
+}
