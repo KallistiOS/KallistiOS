082555d96d94a9225a27ec28a66e33eaaa5ea7d8
QuzarDC <quzar@completext.com>
2026-06-29 16:39:08 -0400

    vblank: Modernize the handle system.
    Rather than an id, have a properly typed pointer returned
    to the handler. This shrinks the footprint of the struct,
    ensures we're passed something to remove that was given out
    during add, and makes it so we don't have to search through
    the list on remove. More in keeping with our current practices.
    
    This does break the API, but it's a trivial change on the user
    side (change int->vblhnd_t*) and is something primarily for
    internal use.

--- kernel/arch/dreamcast/hardware/vblank.c
+++ kernel/arch/dreamcast/hardware/vblank.c
@@ -21,18 +21,16 @@
 /* Our list of handlers */
 struct vblhnd {
     TAILQ_ENTRY(vblhnd) listent;
-    int         id;
     asic_evt_handler    handler;
     void *data;
 };
 static TAILQ_HEAD(vhlist, vblhnd) vblhnds;
-static int vblid_high;
 
 static mutex_t vbl_tailq_mutex = MUTEX_INITIALIZER;
 
 /* Our internal IRQ handler */
 static void vblank_handler(uint32_t src, void *data) {
-    struct vblhnd *t;
+    vblhnd_t *t;
 
     (void)data;
 
@@ -41,22 +39,18 @@ static void vblank_handler(uint32_t src, void *data) {
     }
 }
 
-int vblank_handler_add(asic_evt_handler hnd, void *data) {
-    struct vblhnd *vh = malloc(sizeof(struct vblhnd));
+vblhnd_t *vblank_handler_add(asic_evt_handler hnd, void *data) {
+    vblhnd_t *vh = malloc(sizeof(vblhnd_t));
 
     if(!vh) {
         errno = ENOMEM;
-        return -1;
+        return NULL;
     }
 
     /* Ensure thread safety for tailq access */
     mutex_lock_scoped(&vbl_tailq_mutex);
     /* Disable events until we're done manipulating the tailq */
     asic_evt_disable_scoped(ASIC_EVT_PVR_VBLANK_BEGIN, ASIC_IRQ_DEFAULT);
-
-    /* Find a new ID */
-    vh->id = vblid_high;
-    vblid_high++;
 
     /* Finish filling the struct */
     vh->handler = hnd;
@@ -65,33 +59,23 @@ int vblank_handler_add(asic_evt_handler hnd, void *data) {
     /* Add it to the list */
     TAILQ_INSERT_TAIL(&vblhnds, vh, listent);
 
-    return vh->id;
+    return vh;
 }
 
-int vblank_handler_remove(int handle) {
-    struct vblhnd *t;
-
+void vblank_handler_remove(vblhnd_t *handle) {
     /* Ensure thread safety for tailq access */
     mutex_lock_scoped(&vbl_tailq_mutex);
     /* Disable events until we're done manipulating the tailq */
     asic_evt_disable_scoped(ASIC_EVT_PVR_VBLANK_BEGIN, ASIC_IRQ_DEFAULT);
 
-    /* Look for it */
-    TAILQ_FOREACH(t, &vblhnds, listent) {
-        if(t->id == handle) {
-            TAILQ_REMOVE(&vblhnds, t, listent);
-            free(t);
-            return 0;
-        }
-    }
-    return -1;
+    TAILQ_REMOVE(&vblhnds, handle, listent);
+    free(handle);
 }
 
 int vblank_init(void) {
     /* Setup our data structures */
     TAILQ_INIT(&vblhnds);
     mutex_init(&vbl_tailq_mutex, MUTEX_TYPE_NORMAL);
-    vblid_high = 1;
 
     /* Hook and enable the interrupt */
     asic_evt_set_handler(ASIC_EVT_PVR_VBLANK_BEGIN, vblank_handler, NULL);
@@ -101,7 +85,7 @@ int vblank_init(void) {
 }
 
 int vblank_shutdown(void) {
-    struct vblhnd * c, * n;
+    vblhnd_t *c, *n;
 
     /* Disable and unhook the interrupt */
     asic_evt_disable(ASIC_EVT_PVR_VBLANK_BEGIN, ASIC_IRQ_DEFAULT);
