6a5bc4e114a4535687ad66886df048fe72073a59
darc <darcagn@protonmail.com>
2026-06-30 15:55:32 -0500

    kos-chain: Bump binutils to 2.46.1 for most toolchain profiles

--- utils/kos-chain/profiles/gamecube/stable.mk
+++ utils/kos-chain/profiles/gamecube/stable.mk
@@ -6,7 +6,7 @@ cpu_configure_args=--with-endian=big --with-cpu=750 --disable-multilib --disable
 newlib_extra_configure_args += --disable-libgloss
 
 # Toolchain versions for PowerPC
-binutils_ver=2.45.1
+binutils_ver=2.46.1
 gcc_ver=15.2.0
 newlib_ver=4.6.0.20260123
 
