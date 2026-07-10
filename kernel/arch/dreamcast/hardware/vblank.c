7b519373de8ea09811f7f8640d04badb1469176a
QuzarDC <quzar@completext.com>
2026-07-10 09:54:35 -0400

    vlblank: Small cleanups
    - Don't set ENOMEM as that is set by malloc on fail.
    - Explicitly include <dc/asic.h>.
    - Better spacing in struct vblhnd.

--- kernel/arch/dreamcast/hardware/vblank.c
+++ kernel/arch/dreamcast/hardware/vblank.c
@@ -6,10 +6,10 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <sys/queue.h>
 
 #include <kos/irq.h>
+#include <dc/asic.h>
 #include <dc/vblank.h>
 
 /*
@@ -22,7 +22,7 @@
 struct vblhnd {
     TAILQ_ENTRY(vblhnd) listent;
     asic_evt_handler    handler;
-    void *data;
+    void                *data;
 };
 static TAILQ_HEAD(vhlist, vblhnd) vblhnds;
 
@@ -40,10 +40,7 @@ static void vblank_handler(uint32_t src, void *data) {
 vblhnd_t *vblank_handler_add(asic_evt_handler hnd, void *data) {
     vblhnd_t *vh = malloc(sizeof(vblhnd_t));
 
-    if(!vh) {
-        errno = ENOMEM;
-        return NULL;
-    }
+    if(!vh) return NULL;
 
     /* Disable ints just in case */
     irq_disable_scoped();
